19dc4c0fa3c4e540777485711746c6dde0ed7281
Theo Fortune <theofortunefunction@gmail.com>
2024-01-05 20:29:48 +0100

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, StatusBar } from "react-native";
+import pokemonList from "./data.json";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView style={styles.scrollView}>
+        {pokemonList.map((pokemon) => {
+          return (
+            <View style={styles.card} key={pokemon.id}>
+              <Text style={styles.cardText}>{pokemon.type}</Text>
+              <Text style={styles.cardText}>{pokemon.name}</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +21,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f5f5f5",
+    paddingTop: StatusBar.currentHeight,
   },
+  scrollView: {
+    paddingHorizontal: 16,
+  },
+  card: {
+    backgroundColor: "white",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    marginBottom: 16,
+  },
+  cardText: {
+    fontSize: 30,
+  }
 });
